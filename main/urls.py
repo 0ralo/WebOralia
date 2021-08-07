a0c760651f077dcda02b9a065cf4caa29bff0213
oralo <Ora1o@yandex.ru>
2021-08-07 14:13:01 +0300

    fixed unexist import

--- main/urls.py
+++ main/urls.py
@@ -1,16 +1,14 @@
 from django.urls import path
 
-from main.views import HomeView, PostsView, Summary, NewPostsView, NewCode, SecretView, RandomImage, BestGirl, handler500
+from main.views import HomeView, PostsView, Summary, NewPostsView, NewCode, SecretView, RandomImage
 
 
 urlpatterns = [
 	path('', HomeView.as_view(), name="home"),
-	path('500/', handler500),
 	path('posts/', PostsView.as_view(), name="lent"),
 	path('summary/', Summary.as_view(), name="summary"),
 	path('secret/', SecretView.as_view(), name="secret"),
 	path('newpost/', NewPostsView.as_view(), name="newpost"),
 	path('new_code/', NewCode.as_view(), name="get_invite_code"),
 	path('pic/', RandomImage.as_view(), name="randomimage"),
-	path('bestgirl/', BestGirl.as_view(), name="bestgirl")
 ]
