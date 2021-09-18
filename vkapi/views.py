d8581b9c230b3f17947b05a58d107bd0b9349284
oralo <Ora1o@yandex.ru>
2021-09-18 17:53:10 +0300

    fixed prev commit

--- vkapi/views.py
+++ vkapi/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
 key: str = "283bdf73"
 
 
+@csrf_exempt
 def root(request):
 	if request.POST.get("type", None) == "confirmation" and request.POST.get("group_id", None) == 183943330:
 		return HttpResponse(key)
