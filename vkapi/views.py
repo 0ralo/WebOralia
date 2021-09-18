bf024db7c2b451aae40321287f83ace7e16eedf8
oralo <Ora1o@yandex.ru>
2021-09-18 18:03:28 +0300

    fixed prev commit

--- vkapi/views.py
+++ vkapi/views.py
@@ -1,3 +1,4 @@
+import loguru
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
@@ -7,9 +8,5 @@ key: str = "28bd1796"
 
 @csrf_exempt
 def root(request):
-	if request.POST.get("type", None) == "confirmation" and request.POST.get("group_id", None) == "183943330":
-		return HttpResponse(key)
-	else:
-		rsp = HttpResponse("ERROR")
-		rsp.status_code = 404
-		return rsp
+	loguru.logger.info(request)
+	return HttpResponse(key)
